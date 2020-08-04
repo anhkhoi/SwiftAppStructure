f2a2b80b7743e9aecdd7517889181d12d92cd846
Khoi Tran <anhkhoi.2389@gmail.com>
2020-08-04 18:00:11 +0700

    Feed view controller

---
+++ SwiftAppStructure/Modules/Feed/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  SwiftAppStructure
+//
+//  Created by Khoi Tran on 8/4/20.
+//  Copyright © 2020 Khoi Tran. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
